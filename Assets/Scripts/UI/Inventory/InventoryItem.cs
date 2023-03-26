9f33b79b610a396707a8fabae0d2bebcbd6a7485
JohnyBreak <rashkost@mail.ru>
2023-03-26 17:10:50 +0700

    UI Resource Item Increase Animation

--- Assets/Scripts/UI/Inventory/InventoryItem.cs
+++ Assets/Scripts/UI/Inventory/InventoryItem.cs
@@ -9,14 +9,23 @@ public class InventoryItem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _amountText;
     [SerializeField] private Image _icon;
 
+    private int _increaseHash = Animator.StringToHash("Increase");
+    private Animator _anim;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
     public void Init(Sprite icon, int amount) 
     {
         _icon.sprite = icon;
         SetAmount(amount);
     }
 
-    public void SetAmount(int amount) 
+    public void SetAmount(int amount)
     {
         _amountText.text = amount.ToString();
+        _anim.SetTrigger(_increaseHash);
     }
 }
